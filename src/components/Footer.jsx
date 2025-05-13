1db707f18e8276efdcb18750871e1afdd68a9ed8
prjamming <124306301+prjamming@users.noreply.github.com>
2025-05-13 08:50:08 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <Text weight="bold" align="center" color="gray" size={{ initial: "1", md: "2" }} asChild>
       <footer>
         Did you know? This chatbot runs entirely in your browser. No data is sent to any server.&nbsp;
-        <Link href="https://github.com/nadchif/in-browser-llm-inference/blob/main/HOWTO.md" target="_blank">
+        <Link href="https://github.com/prjamming/wollama/blob/main/HOWTO.md" target="_blank">
           Learn more.
         </Link>
       </footer>
